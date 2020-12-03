8ebd2d36dbbadac5d88dd00900bfca6abc1decfa
iceforz <iceforz3@gmail.com>
2020-12-03 12:43:27 +0300

    123

---
+++ src/test/java/custom/RevertLinkTest.java
@@ -0,0 +1,33 @@
+package custom;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RevertLinkTest {
+
+    @Test
+    public void whenAddThenIter() {
+        RevertLink<Integer> linked = new RevertLink<>();
+        linked.add(1);
+        linked.add(2);
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+    }
+
+    @Test
+    public void whenAddAndRevertThenIter() {
+        RevertLink<Integer> linked = new RevertLink<>();
+        linked.add(1);
+        linked.add(2);
+        linked.revert();
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(1));
+    }
+}
