1d1b0eab46820779cb35e78d8ef379b601f04fbf
iceforz <iceforz3@gmail.com>
2020-12-04 13:37:25 +0300

    2.1. Преобразование даты [#365741]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -3,13 +3,21 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Convert;
 
- public class SqlRuParse {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Pattern;
+
+public class SqlRuParse {
 
     public static void main(String[] args) throws Exception {
+
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements date = doc.select(".altCol");
         Elements row = doc.select(".postslisttopic");
+       String data = conversionDates("dd/MMM/yy", "dd/MM/yy", );
         int index = 1;
         for (Element td : row) {
             Element href = td.child(0);
@@ -19,6 +27,20 @@ import org.jsoup.select.Elements;
             index += 2;
         }
     }
+
+    public static String conversionDates(
+            String inputPattern, String outputPattern, String givenDate) throws ParseException {
+            SimpleDateFormat inputFormat = new SimpleDateFormat(inputPattern);
+            SimpleDateFormat outputFormat = new SimpleDateFormat(outputPattern);
+
+            Date date = null;
+            String requiredDate = null;
+
+            date = inputFormat.parse(givenDate);
+            requiredDate = outputFormat.format(date);
+
+            return requiredDate;
+        }
 }
 
 
