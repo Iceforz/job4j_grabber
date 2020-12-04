3c0fc249efa5464485f8b5a9a2d32d37284eeccb
iceforz <iceforz3@gmail.com>
2020-12-04 16:51:29 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#365737]

---
+++ src/main/java/ru/job4j/html/FivePages.java
@@ -0,0 +1,29 @@
+package ru.job4j.html;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+
+public class FivePages {
+    public static void main(String[] args) throws IOException {
+        printPages(5);
+    }
+
+    public static void printPages(int repeat) throws IOException {
+        String name = "https://www.sql.ru/forum/job-offers/";
+        for (int i = 1; i <= repeat; i++) {
+            Document doc = Jsoup.connect(name + i).get();
+            Elements date = doc.select(".altCol");
+            Elements row = doc.select(".postslisttopic");
+            int index = 1;
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(date.get(index).text());
+                index += 2;
+            }
+        }
+    }
+}
