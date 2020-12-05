d6067eb91c8cccf02ef38343c2605e6c6f397982
iceforz <iceforz3@gmail.com>
2020-12-05 15:05:19 +0300

    2.3. Загрузка деталей поста. [#365739]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -3,10 +3,16 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class SqlRuParse {
+    public static void main(String[] args) throws IOException {
+        printJobsList(5);
+        System.out.println(getJobInfo("https://www.sql.ru/forum/1331208/"));
+    }
 
-    public static void main(String[] args) throws Exception {
+    public static void printJobsList(int repeats) throws IOException {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements date = doc.select(".altCol");
         Elements row = doc.select(".postslisttopic");
@@ -19,8 +25,21 @@ public class SqlRuParse {
             index += 2;
         }
     }
-}
 
+    public static Post getJobInfo(String url) throws IOException {
+        Document doc = Jsoup.connect(url)
+                .get();
+        Elements header = doc.select(".messageHeader");
+        Elements body = doc.select(".msgBody");
+        Elements footer = doc.select(".msgFooter");
+        String title = header.first().text();
+        String desc = body.get(1).text();
+        LocalDateTime date = RussianDate.convert(footer.first()
+                .text()
+                .split("\\s\\[")[0]);
+        return new Post(title, desc, date, url);
+    }
+}
 
 
 
