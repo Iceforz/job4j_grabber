901dfeb5a78e1fc398aac51a264b05cbc2b0ce53
iceforz <iceforz3@gmail.com>
2020-12-05 14:33:44 +0300

    2.1. Преобразование даты [#365741]

---
+++ src/main/java/ru/job4j/html/RussianDate.java
@@ -0,0 +1,50 @@
+package ru.job4j.html;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RussianDate {
+
+    private static Map<String, Integer> months = new HashMap<>();
+
+    static {
+       mapsPut();
+    }
+
+    public static LocalDateTime convert(String date) {
+        String[] data = date.split("\\s");
+        if (data.length < 3) {
+            if ("сегодня,".equals(data[0])) {
+                return LocalDate.now()
+                        .atTime(LocalTime.parse(data[1]));
+            } else {
+                return LocalDate.now()
+                        .minusDays(1)
+                        .atTime(LocalTime.parse(data[1]));
+            }
+        }
+        int day = Integer.parseInt(data[0]);
+        int month = months.get(data[1]);
+        int year = 2000 + Integer.parseInt(data[2].substring(0, 2));
+        return LocalDate.of(year, month, day)
+                .atTime(LocalTime.parse(data[3]));
+    }
+
+    private static void mapsPut() {
+        months.put("янв", 1);
+        months.put("фев", 2);
+        months.put("мар", 3);
+        months.put("апр", 4);
+        months.put("май", 5);
+        months.put("июн", 6);
+        months.put("июл", 7);
+        months.put("авг", 8);
+        months.put("сен", 9);
+        months.put("окт", 10);
+        months.put("ноя", 11);
+        months.put("дек", 12);
+    }
+}
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,87 +4,23 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import javax.print.DocFlavor;
-import java.io.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.regex.Pattern;
-
 public class SqlRuParse {
 
     public static void main(String[] args) throws Exception {
-
-        FileOutputStream file = new FileOutputStream("./src/dates.txt");
-        TeePrintStream tee = new TeePrintStream(file, System.out);
-        System.setOut(tee);
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements date = doc.select(".altCol");
         Elements row = doc.select(".postslisttopic");
-        //   String data = conversionDates("dd/MMM/yy", "dd/MM/yy", "reader");
         int index = 1;
         for (Element td : row) {
             Element href = td.child(0);
-            //System.out.println(href.attr("href"));
-           // System.out.println(href.text());
-            System.out.println(date.get(index).text());
-         //   System.out.println(data);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+            System.out.println(RussianDate.convert(date.get(index).text()));
             index += 2;
         }
     }
-
-    public static String conversionDates(
-            String inputPattern, String outputPattern, String givenDate) throws ParseException {
-        SimpleDateFormat inputFormat = new SimpleDateFormat(inputPattern);
-        SimpleDateFormat outputFormat = new SimpleDateFormat(outputPattern);
-
-        Date date = null;
-        String requiredDate = null;
-
-        date = inputFormat.parse(givenDate);
-        requiredDate = outputFormat.format(date);
-
-        return requiredDate;
-    }
-
-    public static class TeePrintStream extends PrintStream {
-        private final PrintStream second;
-
-        public TeePrintStream(OutputStream main, PrintStream second) {
-            super(main);
-            this.second = second;
-        }
-
-        @Override
-        public void close() {
-            super.close();
-        }
-
-        @Override
-        public void flush() {
-            super.flush();
-            second.flush();
-        }
-
-        @Override
-        public void write(byte[] buf, int off, int len) {
-            super.write(buf, off, len);
-            second.write(buf, off, len);
-        }
-
-        @Override
-        public void write(int b) {
-            super.write(b);
-            second.write(b);
-        }
-
-        @Override
-        public void write(byte[] b) throws IOException {
-            super.write(b);
-            second.write(b);
-        }
-    }
 }
+
 
 
 
