1c41341e10b44e0fb6ee782c86d1769f410c60fb
iceforz <iceforz3@gmail.com>
2020-12-08 14:39:13 +0300

    7. Web. [#365743]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -5,6 +5,9 @@ import org.quartz.impl.StdSchedulerFactory;
 import ru.job4j.html.*;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -38,6 +41,28 @@ public class Grabber implements Grab {
         private void setUrl() {
             url = cfg.getProperty("url");
         }
+
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(
+                    Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes());
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
 
         @Override
         public void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException {
@@ -78,6 +103,7 @@ public class Grabber implements Grab {
             Scheduler scheduler = grab.scheduler();
             Store store = grab.store();
             grab.init(new SqlRuParser(), store, scheduler);
+            grab.web(store);
         }
     }
 
