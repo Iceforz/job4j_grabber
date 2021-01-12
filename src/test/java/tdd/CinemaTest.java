1efa995197c78bcea83ac496eaa11c915acf48dd
iceforz <iceforz3@gmail.com>
2021-01-12 18:53:49 +0300

    2. Что такое TDD? [#365844]

---
+++ src/test/java/tdd/CinemaTest.java
@@ -0,0 +1,47 @@
+package tdd;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Ignore;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CinemaTest {
+
+    @Ignore
+    public void buy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Ignore
+    public void find() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    @Ignore
+    public void whenNoTickets() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertNull(ticket);
+    }
+
+    @Ignore
+    public void whenNoSessions() {
+        Cinema cinema = new Cinema3D();
+        List<Session> sessions = cinema.find(session -> true);
+        assertTrue(sessions.size() == 0);
+    }
+}
