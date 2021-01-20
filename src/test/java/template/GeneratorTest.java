042d8f12607df680e28c46c72e967246a2463e3b
iceforz <iceforz3@gmail.com>
2021-01-20 13:52:50 +0300

    3. Шаблонизатор. [#365843]

--- src/test/java/template/GeneratorTest.java
+++ src/test/java/template/GeneratorTest.java
@@ -1,15 +1,21 @@
 package template;
 
-import org.junit.jupiter.api.Test;
 
+//import org.junit.jupiter.api.Test;
+import java.lang.IllegalArgumentException;
 import java.util.Map;
+
+import org.junit.Ignore;
+import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
- class GeneratorTest {
+public class GeneratorTest {
 
     @Test
+    @Ignore
     public void whenWork() {
         Generator generator = new TGenerator();
         String temp = "Hello ${name}, how you ${subject}?";
@@ -23,14 +29,17 @@ import static org.junit.Assert.*;
     }
 
     @Test
+    @Ignore
     public void whenNotEnough() {
         Generator generator = new TGenerator();
         String temp = "Hello ${name}, how you ${subject}?";
         Map<String, String> map = Map.of("name", "Alex");
-        generator.produce(temp, map);
+        assertThrows(IllegalArgumentException.class, () ->
+                generator.produce(temp, map));
     }
 
     @Test
+    @Ignore
     public void whenTooMany() {
         Generator generator = new TGenerator();
         String temp = "Hello ${name}, how you ${subject}?";
@@ -39,6 +48,7 @@ import static org.junit.Assert.*;
                 "subject", "doing",
                 "anotherSubject", "buddy"
         );
-        generator.produce(temp, map);
+        assertThrows(IllegalArgumentException.class, () ->
+                generator.produce(temp, map));
     }
 }
