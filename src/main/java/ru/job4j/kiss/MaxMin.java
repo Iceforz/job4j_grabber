b816df9bd6f0906c37bc124ee2b90d55406efc16
iceforz <iceforz3@gmail.com>
2021-01-20 14:57:20 +0300

    1. Принципы Kiss, Dry и Yagni [#365842]

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -2,21 +2,29 @@ package ru.job4j.kiss;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class MaxMin {
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        T max = value.get(0);
-        for (int i = 0; i < value.size() - 1; i++) {
-            max = comparator.compare(max, value.get(i + 1)) >= 0  ? max : value.get(i + 1);
-        }
-        return max;
+        Predicate<Integer> predicate = integer -> integer < 0;
+        return find(value, comparator, predicate);
     }
 
-        public <T> T min(List<T> value, Comparator<T> comparator) {
-            T min = value.get(0);
-            for (int i = 0; i < value.size() - 1; i++) {
-                min = comparator.compare(min, value.get(i + 1)) <= 0  ? min : value.get(i + 1);
-            }
-            return min;
+    public <T> T min(List<T> value, Comparator<T> comparator) {
+        Predicate<Integer> predicate = integer -> integer > 0;
+        return find(value, comparator, predicate);
+    }
+
+    private <T> T find(List<T> value, Comparator<T> comparator, Predicate<Integer> predicate) {
+        if (value.size() == 0) {
+            return null;
         }
+        T rsl = value.get(0);
+        for (T elem : value) {
+            if (predicate.test(comparator.compare(rsl, elem))) {
+                rsl = elem;
+            }
+        }
+        return rsl;
+    }
 }
--- src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -3,28 +3,29 @@ package ru.job4j.kiss;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class MaxMinTest {
 
-    private MaxMin maxMin = new MaxMin();
-    private List<Person> personList = Arrays.asList(
-            new Person("Slava", 34),
-            new Person("Vitaly", 23),
-            new Person("Kate", 12)
-    );
-
     @Test
-    public void whenUseAgeComp() {
-        assertThat(maxMin.max(personList, Person.AGECOMP).getAge(), is(34));
-        assertThat(maxMin.min(personList, Person.AGECOMP).getAge(), is(12));
+    public void max() {
+        MaxMin maxMin = new MaxMin();
+        List<String> input = List.of("Slava", "Vitaly", "Kate");
+        String rsl = maxMin.max(input, Comparator.naturalOrder());
+        String expected = "Kate";
+        assertEquals(expected, rsl);
     }
 
     @Test
-    public void whenUseLengthComp() {
-        assertThat(maxMin.max(personList, Person.LENGTHCOMP).getName(), is("Vitaly"));
-        assertThat(maxMin.min(personList, Person.AGECOMP).getName(), is("Kate"));
+    public void min() {
+        MaxMin maxMin = new MaxMin();
+        List<String> input = List.of("Slava", "Vitaly", "Kate");
+        String rsl = maxMin.max(input, Comparator.reverseOrder());
+        String expected = "Vitaly";
+        assertEquals(expected, rsl);
     }
 }
